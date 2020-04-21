97a901bd7478ba513f0118f9ad7b39f988a277c9
HeeBeom Wang <60314278+kt3472@users.noreply.github.com>
2020-04-21 22:24:09 +0900

    Add files via upload

---
+++ Feature상관관계.R
@@ -0,0 +1,31 @@
+library(igraph)
+source('11-2.FeatureSetTA(2).R')
+printf <- function(...) cat(sprintf(...))
+
+# 기술적 지표들의 상관관계를 관찰한다.
+# Yahoo 사이트로부터 삼성전자 주가 데이터를 읽어온다
+p <- getData('005930')
+
+# 기술적 분석 Feature 데이터 세트를 생성한다
+ds <- FeatureSetTA2(p)
+myClass <- ds$train$class
+ds$train$class <- NULL
+
+# Feature 들 간의 상관계수를 구한다 (상관계수 행렬)
+cm <- cor(ds$train)
+print(cm)
+
+# 상관계수 행렬을 그래프 형태로 그린다. 상관계수가 0.3 이상인 것.
+g <- graph.adjacency(cm > 0.3, weighted=TRUE, mode="upper", diag = FALSE)
+
+par(mai=c(1,1,0.1,0.15), mar=c(1, 0, 1, 1), mgp=c(2,1,0), mfrow=c(1,1), cex=1, lwd=1)
+plot(g, vertex.size=25, vertex.color='green')
+
+# SMI ~ MACD의 상관관계 확인
+par(mfrow=c(1,2), mar=c(3, 3, 2, 2), mgp=c(1.5, 0.3, 0))
+title <- sprintf("Correlation = %.4f", cor(ds$train$smi, ds$train$macd))
+plot(x=ds$train$smi, y=ds$train$macd, xlim=c(-3,2.5), ylim=c(-3,2.5), xlab="SMI", ylab="MACD", main=title, pch=c(4,20)[unclass(myClass)], col=c('red', 'blue')[unclass(myClass)])
+
+# ATR ~ VOLATILITy의 상관관계 확인
+title <- sprintf("Correlation = %.4f", cor(ds$train$atr, ds$train$volatility))
+plot(x=ds$train$atr, y=ds$train$volatility, xlim=c(-1.5,3), ylim=c(-1.5,3), xlab="ATR", ylab="Volatility", main=title, pch=c(4,20)[unclass(myClass)], col=c('red', 'blue')[unclass(myClass)])
---
+++ Feature조합.R
@@ -0,0 +1,45 @@
+library(e1071)
+source('11-2.FeatureSetTA(2).R')
+printf <- function(...) cat(sprintf(...))
+
+# 기술적 분석 Feature들을 조합하여 정확도를 측정해 본다.
+# ex : 
+# 총 11개의 Feature들로 구성된 데이터 세트에서 k개의 Feature들을 조합하여 
+# 데이터 세트를 구성한다. k=5개를 선택하는 조합의 개수는 462개 임.
+# 
+# 1번 조합 : ATR, SMI, ADX, Bollinger, MACD --> 57.38%
+# 2번 조합 : ATR, SMI, ADX, Bollinger, OBV --> 54.25% 등
+# 
+# k = 6, 7, 8 ... 11 인 경우의 모든 조합으로 정확도를 계산해 보고, 의미있는
+# Feature 조합이 있는지 평가해 본다. (종목도 바꿔가면서 평가해 보아야 함.)
+# -------------------------------------------------------------------------
+p <- getData('005930')
+
+k = 8
+c <- as.data.frame(t(combn(11, k)))
+c$accuracy <- NA
+
+for (i in 1:nrow(c)) {
+   com <- rep(0, 11)
+   r <- as.numeric(c[i,])
+   com[r] <- 1
+
+   # 기술적 분석 Feature 데이터 세트를 생성한다
+   ds <- FeatureSetTA2(p, com)
+   
+   # SVM 을 생성한다
+   sv <- svm(class ~ ., data = ds$train, kernel="radial", cost=100, gamma=0.1)
+   
+   # 테스트 데이터 세트를 이용하여 성능을 확인한다
+   pred <- predict(sv, ds$test, type = "class")
+   cm <- table(pred, ds$test$class, dnn=list('predicted', 'actual'))
+   accuracy <- sum(diag(cm)) / sum(cm)
+   c[i,]$accuracy <- accuracy
+   printf("%d : 정확도 = %.4f\n", i, accuracy)
+}
+
+# Feature 선택에 따른 정확도의 분산을 육안으로 확인한다
+title <- sprintf("k = %d, 평균 = %.2f, 표준편차 = %.2f %s", k, mean(c$accuracy)*100, sd(c$accuracy)*100, "%")
+plot(density(c$accuracy), col='red', main=title)
+abline(v=0.5, col='blue')
+
